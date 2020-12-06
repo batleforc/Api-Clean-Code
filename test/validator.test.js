f3d93fac422612cd737d64c2ac1216b1c4149d90
Maxime Leriche <maxleriche.60@gmail.com>
2020-12-06 02:46:13 +0100

    Let's go to far

--- test/validator.test.js
+++ test/validator.test.js
@@ -17,4 +17,10 @@ test('Test not null or undefined of an integer (true mode)',()=>{
 test('Test  not null or undefined of an integer (false mode) ',()=>{
     expect(validator.notNullOrUndefinedOranInteger(null)).toBe(false)
     expect(validator.notNullOrUndefinedOranInteger(undefined)).toBe(false)
+    expect(validator.notNullOrUndefinedOranInteger(NaN)).toBe(false)
+    expect(validator.notNullOrUndefinedOranInteger([])).toBe(false)
+    expect(validator.notNullOrUndefinedOranInteger({})).toBe(false)
+    expect(validator.notNullOrUndefinedOranInteger()).toBe(false)
+    expect(validator.notNullOrUndefinedOranInteger(()=>{})).toBe(false)
+
 })
