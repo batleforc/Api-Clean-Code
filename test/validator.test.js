6207e67d44009e94d8fb4d0f8e6b2f3556db6ec1
Maxime Leriche <maxleriche.60@gmail.com>
2020-12-06 02:42:13 +0100

    Tentative d'ajout de test false (POURQUOI CA PASSE PASSSS

--- test/validator.test.js
+++ test/validator.test.js
@@ -1,6 +1,6 @@
 const validator = require('../helper/validator')
 
-test('Test not null or undefined or an integer',()=>{
+test('Test not null or undefined of an integer (true mode)',()=>{
     expect(validator.notNullOrUndefinedOranInteger(10)).toBe(true)
     expect(validator.notNullOrUndefinedOranInteger("10")).toBe(true)
     expect(validator.notNullOrUndefinedOranInteger(1)).toBe(true)
@@ -12,4 +12,9 @@ test('Test not null or undefined or an integer',()=>{
 
     expect(validator.notNullOrUndefinedOranInteger(Math.PI)).toBe(true)
     expect(validator.notNullOrUndefinedOranInteger(`${Math.PI}`)).toBe(true)
+})
+
+test('Test  not null or undefined of an integer (false mode) ',()=>{
+    expect(validator.notNullOrUndefinedOranInteger(null)).toBe(false)
+    expect(validator.notNullOrUndefinedOranInteger(undefined)).toBe(false)
 })
