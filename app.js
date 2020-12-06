27f1ba375c53574e0b33ee83204eee9145b63076
Maxime Leriche <maxleriche.60@gmail.com>
2020-12-06 01:47:43 +0100

    Add comment... i love comments

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express');
 const compression = require('compression');
 var router = require('./routes/apiRouter').router
+/**
+ * * Classe API, this api handle all the workflow of the api and bind use the needed path
+ * * Please remember the fact that this api use a GZIP compression that allow to reduce the size of response 
+ */
 class Api{
     app
     constructor(port){
         this.app = express()
+        /**
+         * * Set up of the header, this part allow the api to be called from localhost and only localhost, 
+         * * This part allow some special verbose like get,post,options,put,patch and delete
+         */
         this.app.use((req,res,next)=>{
             res.setHeader('Access-Control-Allow-Origin',`localhost:${port}`);
             res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -14,16 +22,23 @@ class Api{
         
         this.app.use(express.json());
         this.app.use(compression());
-        this.app.use('/',router)
-        
+        /**
+         * * Path that stick to the index and say to the user to go to the path needed by the TP
+         */
         this.app.get('/',(req,res)=>{
             res.status(404).json({
                 status:"Nothing here",
-                message:"I think you need to go to /client/scanner/generate if you want to find something...but if you want i'm a teapot"
+                message:"I think you need to go to /client/scanner/generate if you want to find something..."
             })
         })
         /**
-         * Path that handle every other wrong path
+         * * Add of the router to the index path
+         */
+        this.app.use('/',router)
+        
+        
+        /**
+         * * Path that handle every other wrong path
          */
         this.app.use((req,res)=>{
             res.status(418).json({
@@ -33,6 +48,9 @@ class Api{
             })
         })
     }
+    /**
+     * * Create a fonction that return the App
+     */
     getApp = ()=> this.app
 }
 
